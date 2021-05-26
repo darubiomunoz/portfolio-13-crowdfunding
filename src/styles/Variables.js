b950562bbc0775537097999f1678e7b548c5dfd8
Diego Rubio <darubiomunoz@gmail.com>
2021-05-26 17:58:30 -0400

    Styled components variables

---
+++ src/styles/Variables.js
@@ -0,0 +1,85 @@
+export const colorOption1 = `hsl(176, 50%, 47%)`;
+export const colorOption2 = `hsl(176, 72%, 28%)`;
+export const colorOption3 = `hsl(0, 0%, 48%)`;
+export const colorOption4 = `hsl(0, 0%, 0%)`;
+export const colorOption5 = `#FFF`;
+
+export const fontFamily = `'Commissioner', sans-serif`;
+
+export const fontSizeSmall = `10px`;
+export const fontSizeRegular = `12px`;
+export const fontSizeNormal = `16px`;
+export const fontSizeMedium = `24px`;
+export const fontSizeLarge = `32px`;
+export const fontSizeExtraLarge = `48px`;
+
+export const fontWeightExtraLight = `200`;
+export const fontWeightLight = `300`;
+export const fontWeightNormal = `400`;
+export const fontWeightBold = `700`;
+export const fontWeightExtraBold = `900`;
+
+export const breakpointWidth240 = `(min-width: 240px)`;
+export const breakpointWidth280 = `(min-width: 280px)`;
+export const breakpointWidth320 = `(min-width: 320px)`;
+export const breakpointWidth360 = `(min-width: 360px)`;
+export const breakpointWidth375 = `(min-width: 375px)`;
+export const breakpointWidth384 = `(min-width: 384px)`;
+export const breakpointWidth411 = `(min-width: 411px)`;
+export const breakpointWidth412 = `(min-width: 412px)`;
+export const breakpointWidth414 = `(min-width: 414px)`;
+export const breakpointWidth480 = `(min-width: 480px)`;
+export const breakpointWidth540 = `(min-width: 540px)`;
+export const breakpointWidth600 = `(min-width: 600px)`;
+export const breakpointWidth768 = `(min-width: 768px)`;
+export const breakpointWidth800 = `(min-width: 800px)`;
+export const breakpointWidth1024 = `(min-width: 1024px)`;
+export const breakpointWidth1280 = `(min-width: 1280px)`;
+export const breakpointWidth1440 = `(min-width: 1440px)`;
+
+export const device = {
+  width240: breakpointWidth240,
+  width280: breakpointWidth280,
+  width320: breakpointWidth320,
+  width360: breakpointWidth360,
+  width375: breakpointWidth375,
+  width384: breakpointWidth384,
+  width411: breakpointWidth411,
+  width412: breakpointWidth412,
+  width414: breakpointWidth414,
+  width480: breakpointWidth480,
+  width540: breakpointWidth540,
+  width600: breakpointWidth600,
+  width768: breakpointWidth768,
+  width800: breakpointWidth800,
+  width1024: breakpointWidth1024,
+  width1280: breakpointWidth1280,
+  width1440: breakpointWidth1440,
+};
+
+export const spacingExtraSmall = `5px`;
+export const spacingSmall = `10px`;
+export const spacingRegular = `20px`;
+export const spacingNormal = `30px`;
+export const spacingMedium = `40px`;
+export const spacingLarge = `50px`;
+export const spacingExtraLarge = `60px`;
+
+export const borderRadiusExtraSmall = `5px`;
+export const borderRadiusSmall = `10px`;
+export const borderRadiusRegular = `20px`;
+export const borderRadiusNormal = `30px`;
+export const borderRadiusLarge = `40px`;
+export const borderRadiusExtraLarge = `50px`;
+
+export const boxShadow = `0px 3px 10px 0px rgba(173,164,164,0.5);`;
+
+export const zIndexBackground = `0`;
+export const zIndexNormal = `10`;
+export const zIndexDropDown = `1000`;
+export const zIndexSticky = `1010`;
+export const zIndexFixed = `1020`;
+export const zIndexModalBackDrop = `1030`;
+export const zIndexModal = `1040`;
+export const zIndexPopOver = `1050`;
+export const zIndexToolTip = `1060`;
