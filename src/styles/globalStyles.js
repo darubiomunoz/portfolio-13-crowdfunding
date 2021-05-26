90f104943d60791694e51797d55820157e91a121
Diego Rubio <darubiomunoz@gmail.com>
2021-05-26 18:46:51 -0400

    Adding more global styles

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,8 +2,10 @@ import { createGlobalStyle } from 'styled-components';
 
 import { fontFamily } from './variables'
 
-const GlobalStyle =createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
 *{
+  padding: 0;
+  margin: 0;
   font-size: 62.5%;
   font-family: fontFamily;
   box-sizing: border-box;
