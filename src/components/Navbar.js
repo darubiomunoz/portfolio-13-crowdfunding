4708a2f514e5f9fd7d6d7aa671bcf29766be1b39
Diego Rubio <darubiomunoz@gmail.com>
2021-05-27 14:54:49 -0400

    Adding logic to open or close the navigation menu and change the icon when opened or closed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,27 @@
+import { useState } from 'react';
+
 import NavbarContainer from '../styles/components/Navbar/NavbarContainer';
 import MenuContainer from '../styles/components/Navbar/MenuContainer';
 
 import logo from "../assets/logos/logo.svg";
 import burgerIcon from '../assets/icons/icon-hamburger.svg';
+import closeIcon from '../assets/icons/icon-close-menu.svg'
 
 const Navbar = () => {
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <NavbarContainer>
       <figure>
         <img src={logo} alt="Crowdfund logotype" />
       </figure>
       <MenuContainer>
-        <figure>
-          <img src={burgerIcon} alt="Burger Icon. Press enter to open the navigation menu" />
+        <figure onClick={handleClick}>
+          <img src={!isOpen ? burgerIcon : closeIcon} alt="Burger Icon. Press enter to open the navigation menu" />
         </figure>
       </MenuContainer>
     </NavbarContainer>
