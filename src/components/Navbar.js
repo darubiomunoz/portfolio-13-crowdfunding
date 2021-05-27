ee7204e27f5026e5a17e8cd116dd889994c46eda
Diego Rubio <darubiomunoz@gmail.com>
2021-05-27 15:26:13 -0400

    Layout in JSX of the dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { nanoid } from '@reduxjs/toolkit';
 
 import NavbarContainer from '../styles/components/Navbar/NavbarContainer';
 import MenuContainer from '../styles/components/Navbar/MenuContainer';
+import Dropdown from '../styles/components/Navbar/Dropdown';
 
 import logo from "../assets/logos/logo.svg";
 import burgerIcon from '../assets/icons/icon-hamburger.svg';
@@ -23,8 +25,22 @@ const Navbar = () => {
       </figure>
       <MenuContainer>
         <figure onClick={handleClick}>
-          <img src={!isOpen ? burgerIcon : closeIcon} alt="Burger Icon. Press enter to open the navigation menu" />
+          <img
+            src={!isOpen ? burgerIcon : closeIcon}
+            alt="Burger Icon. Press enter to open the navigation menu"
+          />
         </figure>
+        <Dropdown>
+          <ul>
+            {navigationOptions.map((option) => {
+              return (
+                <li key={nanoid()}>
+                  <a href="">{option}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </Dropdown>
       </MenuContainer>
     </NavbarContainer>
   );
---
+++ src/styles/components/Navbar/Dropdown.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Dropdown = styled.div`
+
+`;
+
+export default Dropdown;
