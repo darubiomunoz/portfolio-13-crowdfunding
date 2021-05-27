7ec31a50c99445f3fe970bdcb5b9f2f46edaf204
Diego Rubio <darubiomunoz@gmail.com>
2021-05-26 20:05:53 -0400

    Adding responsive hero image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,21 @@
+import HomePageContainer from '../styles/pages/HomePageContainer';
+
+import { breakpointWidth1024 } from '../styles/variables';
+
 const HomePage = () => {
   return (
-    <div>r</div>
+    <HomePageContainer>
+      <picture>
+        <source
+          media={breakpointWidth1024}
+          srcSet="https://i.postimg.cc/02yQRXwr/image-hero-desktop.jpg"
+        />
+        <img
+          src="https://i.postimg.cc/85g521RV/image-hero-mobile.jpg"
+          alt="Background picture. Desktop with a desktop computer"
+        />
+      </picture>
+    </HomePageContainer>
   );
 }
 
---
+++ src/styles/pages/HomePageContainer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const HomePageContainer = styled.main`
+
+`;
+
+export default HomePageContainer;
