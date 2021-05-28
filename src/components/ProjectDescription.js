e59fb383c0592034507f29bc14fc15df09894692
Diego Rubio <darubiomunoz@gmail.com>
2021-05-28 12:52:40 -0400

    JSX layout of the project description component

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -1,5 +1,23 @@
+import ProjectDescriptionContainer from "../styles/components/ProjectDescription/ProjectDescriptionContainer";
+
 const ProjectDescription = () => {
-  return (<h1>project description</h1>);
-}
+  return (
+    <ProjectDescriptionContainer>
+      <figure>
+        <img src="" alt="" />
+      </figure>
+      <article>
+        <h1>Mastercraft Bamboo Monitor Riser</h1>
+        <p>A beautiful handcrafted monitor stand to reduce neck and eye strain.</p>
+      </article>
+      <div>
+        <button type="button">Back this project</button>
+        <button type="button">
+          <img src="" alt="" />
+        </button>
+      </div>
+    </ProjectDescriptionContainer>
+  );
+};
 
 export default ProjectDescription;
