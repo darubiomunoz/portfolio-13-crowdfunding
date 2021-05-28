0c8ce05875509b720a3d01199bb234e55632b093
Diego Rubio <darubiomunoz@gmail.com>
2021-05-28 13:33:09 -0400

    Styles for the article section of the project description component

--- src/styles/components/ProjectDescription/ProjectDescriptionContainer.js
+++ src/styles/components/ProjectDescription/ProjectDescriptionContainer.js
@@ -1,14 +1,39 @@
 import styled from 'styled-components';
 
-import { borderRadiusSmall, spacingSmall } from '../../variables';
+import { borderRadiusSmall, colorOption5, fontSizeSmall, fontSizeRegular, fontWeightBold, spacingSmall, fontWeightLight, colorOption3, spacingNormal } from '../../variables';
 
 const ProjectDescriptionContainer = styled.section`
-  width: 90%;
+  width: 87%;
   padding: ${spacingSmall};
   position: relative;
   top: -30px;
   border-radius: ${borderRadiusSmall};
-  background-color: red;
+  background-color: ${colorOption5};
+
+  figure {
+    img {
+    }
+  }
+
+  article {
+    display: flex;
+    flex-direction: column;
+    row-gap: ${spacingSmall};
+    align-items: center;
+    text-align: center;
+
+    h1 {
+      padding: 0 ${spacingNormal};
+      font-size: ${fontSizeRegular};
+      font-weight: ${fontWeightBold};
+    }
+
+    p {
+      font-size: ${fontSizeSmall};
+      font-weight: ${fontWeightLight};
+      color: ${colorOption3};
+    }
+  }
 `;
 
 export default ProjectDescriptionContainer;
