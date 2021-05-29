ce3eb4ccdd470bbd76500f1200e943a9981d634f
Diego Rubio <darubiomunoz@gmail.com>
2021-05-28 21:36:31 -0400

    Project Statistics bar styles

--- src/components/ProjectStatistics.js
+++ src/components/ProjectStatistics.js
@@ -1,5 +1,21 @@
+import ProjectStatisticsContainer from "../styles/components/ProjectStatistics/ProjectStatisticsContainer";
+
 const ProjectStatistics = () => {
-  return (<h1>Project statistics</h1>);
+  return (
+    <ProjectStatisticsContainer>
+      <p className="figure">$89,914</p>
+      <span className="sub-paragraph">of $100,000 backed</span>
+      <span className="divisor"></span>
+      <p className="figure">5,007</p>
+      <span className="sub-paragraph">total backers</span>
+      <span className="divisor"></span>
+      <p className="figure">56</p>
+      <span className="sub-paragraph">days left</span>
+      <div className="progress-bar">
+        <span></span>
+      </div>
+    </ProjectStatisticsContainer>
+  );
 };
 
 export default ProjectStatistics;
---
+++ src/styles/components/ProjectStatistics/ProjectStatisticsContainer.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+import { borderRadiusSmall, boxShadow, colorOption1, colorOption2, colorOption3, colorOption5, fontSizeNormal, fontSizeRegular, fontSizeSmall, fontWeightBold, spacingRegular, spacingSmall } from '../../variables';
+
+const ProjectStatisticsContainer = styled.section`
+  width: 87%;
+  padding: ${spacingRegular} ${spacingSmall};
+  display: flex;
+  flex-direction: column;
+  row-gap: ${spacingSmall};
+  align-items: center;
+  border-radius: ${borderRadiusSmall};
+  background-color: ${colorOption5};
+  box-shadow: ${boxShadow};
+
+  .figure {
+    font-size: ${fontSizeNormal};
+    font-weight: ${fontWeightBold};
+  }
+
+  .sub-paragraph {
+    font-size: ${fontSizeSmall};
+    color: ${colorOption3};
+  }
+
+  .divisor {
+    width: 26%;
+    border-top: 0.1rem solid ${colorOption3};
+  }
+
+  .progress-bar {
+    width: 100%;
+    height: 1rem;
+    display: flex;
+    justify-content: flex-start;
+    border-radius: ${borderRadiusSmall};
+    background-color: ${colorOption3};
+
+    span {
+      width: 80%;
+      height: inherit;
+      border-radius: ${borderRadiusSmall};
+      background-color: ${colorOption1};
+    }
+  }
+`;
+
+export default ProjectStatisticsContainer;
