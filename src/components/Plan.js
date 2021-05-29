07ee8c5a67299a7fac9ef94a3846a1486ceea05c
Diego Rubio <darubiomunoz@gmail.com>
2021-05-29 13:49:51 -0400

    JSX layout of plan component

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -3,7 +3,14 @@ import PlanContainer from '../styles/components/Plan/PlanContainer';
 const Plan = () => {
   return (
     <PlanContainer>
-      wena wena
+      <h2>Bamboo Stand</h2>
+      <h3>Pladge $25 or more</h3>
+      <p>You get an ergonomic stand made of natural bamboo. You've helped us launh our promotional campaign, and you'll be added to a special BAcker member list.</p>
+      <div>
+        <span>101</span>
+        <p>left</p>
+      </div>
+      <button type="button">Select Reward</button>
     </PlanContainer>
   );
 }
