0e4d675229c00aa04d9f8f604719962003af4f91
Diego Rubio <darubiomunoz@gmail.com>
2021-05-29 14:19:37 -0400

    Plan styles

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -3,14 +3,14 @@ import PlanContainer from '../styles/components/Plan/PlanContainer';
 const Plan = () => {
   return (
     <PlanContainer>
-      <h2>Bamboo Stand</h2>
-      <h3>Pladge $25 or more</h3>
-      <p>You get an ergonomic stand made of natural bamboo. You've helped us launh our promotional campaign, and you'll be added to a special BAcker member list.</p>
-      <div>
-        <span>101</span>
-        <p>left</p>
+      <h2 className="plan_title">Bamboo Stand</h2>
+      <h3 className="plan_subtitle">Pledge $25 or more</h3>
+      <p className="plan_paragraph">You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.</p>
+      <div className="plan_counter">
+        <span className="counter_number">101</span>
+        <p className="counter_text">left</p>
       </div>
-      <button type="button">Select Reward</button>
+      <button className="plan_button" type="button">Select Reward</button>
     </PlanContainer>
   );
 }
--- src/styles/components/Plan/PlanContainer.js
+++ src/styles/components/Plan/PlanContainer.js
@@ -1,13 +1,59 @@
 import styled from 'styled-components';
 
-import { borderRadiusExtraSmall, boxShadow, spacingRegular, spacingSmall } from '../../variables';
+import { borderRadiusExtraSmall, borderRadiusRegular, boxShadow, colorOption1, colorOption3, colorOption5, fontSizeMedium, fontSizeNormal, fontSizeSmall, fontWeightBold, fontWeightNormal, spacingRegular, spacingSmall } from '../../variables';
 
 const PlanContainer = styled.div`
   width: 100%;
   margin-top: ${spacingRegular};
   padding: ${spacingSmall};
+  padding-bottom: ${spacingRegular};
   border-radius: ${borderRadiusExtraSmall};
   box-shadow: ${boxShadow};
+
+  .plan_title {
+    font-size: ${fontSizeSmall};
+    font-weight: ${fontWeightBold};
+  }
+
+  .plan_subtitle {
+    font-size: ${fontSizeSmall};
+    font-weight: ${fontWeightNormal};
+    color: ${colorOption1};
+  }
+
+  .plan_paragraph {
+    margin-top: ${spacingRegular};
+    font-size: ${fontSizeSmall};
+    font-weight: ${fontWeightNormal};
+    color: ${colorOption3};
+  }
+
+  .plan_counter {
+    margin-top: ${spacingRegular};
+    display: flex;
+    align-items: center;
+
+    .counter_number {
+      font-size: ${fontSizeMedium};
+      font-weight: ${fontWeightBold};
+    }
+
+    .counter_text {
+      margin-left: ${spacingSmall};
+      font-size: ${fontSizeSmall};
+      color: ${colorOption3};
+    }
+  }
+
+  .plan_button {
+    margin-top: ${spacingRegular};
+    padding: ${spacingSmall} ${spacingRegular};
+    font-size: ${fontSizeSmall};
+    border: none;
+    border-radius: ${borderRadiusRegular};
+    background-color: ${colorOption1};
+    color: ${colorOption5};
+  }
 `;
 
 export default PlanContainer;
