7bb4108280dfb54df8727ab19d35031f27802f9f
Diego Rubio <darubiomunoz@gmail.com>
2021-05-31 15:56:30 -0400

    Using data from API and redux instead of hardcoded info

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,17 @@
+import { useSelector } from "react-redux";
+
 import AboutContainer from '../styles/components/About/AboutContainer';
 
 import Plan from './Plan';
 
 const About = () => {
+  const aboutInfo = useSelector(state => state.data.info[0].about);
+  console.log(aboutInfo);
   return (
     <AboutContainer>
-      <h2 className="about_title">About this project</h2>
-      <p className="about_paragraph">
-        The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform
-        that elevates your screen to a more comfortable viewing height. Placing
-        your monitor eye level has the potential to improve your posture and
-        make you more comfortable while at work, helping you stay focused on the
-        task at hand.
-      </p>
-      <p className="about_paragraph">
-        Featuring artisan craftsmanship, the simplicity of design creates extra
-        desk space below your computer to allow notepads, pens, and USB stikcs
-        to be stored under the stand.
-      </p>
+      <h2 className="about_title">{aboutInfo.title}</h2>
+      <p className="about_paragraph">{aboutInfo.description.paragraph1}</p>
+      <p className="about_paragraph">{aboutInfo.description.paragraph2}</p>
       <Plan />
     </AboutContainer>
   );
