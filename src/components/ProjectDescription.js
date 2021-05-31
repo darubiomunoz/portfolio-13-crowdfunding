b418d72eb93741e9497321c879db982038d85a2d
Diego Rubio <darubiomunoz@gmail.com>
2021-05-31 15:20:09 -0400

    Replacing data hardcoded to use info from API and redux in the projet description component

--- src/components/ProjectDescription.js
+++ src/components/ProjectDescription.js
@@ -1,20 +1,25 @@
+import { useSelector } from 'react-redux';
+
 import ProjectDescriptionContainer from "../styles/components/ProjectDescription/ProjectDescriptionContainer";
 
 import mastercraftLogo from '../assets/logos/logo-mastercraft.svg';
 import bookmarkIcon from '../assets/icons/icon-bookmark.svg';
 
 const ProjectDescription = () => {
+  const projectDescriptionInfo = useSelector(state => state.data.info[0].projectDescription);
+  console.log(projectDescriptionInfo);
+
   return (
-    <ProjectDescriptionContainer id="About">
+    <ProjectDescriptionContainer id="">
       <figure>
         <img src={mastercraftLogo} alt="Mastercraft logotype" />
       </figure>
       <article>
-        <h1>Mastercraft Bamboo Monitor Riser</h1>
-        <p>A beautiful handcrafted monitor stand to reduce neck and eye strain.</p>
+        <h1>{projectDescriptionInfo.name}</h1>
+        <p>{projectDescriptionInfo.purpose}</p>
       </article>
       <div>
-        <button className="backthisproject" type="button">Back this project</button>
+        <button className="backthisproject" type="button">{projectDescriptionInfo.buttons.funding}</button>
         <button className="bookmark" type="button">
           <img src={bookmarkIcon} alt="Bookmark icon. Press enter to bookmark the Mastercraft project" />
         </button>
