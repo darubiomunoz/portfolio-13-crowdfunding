f6a62c41a9dbe2aba2389fcabb3684f5eacc4909
Diego Rubio <darubiomunoz@gmail.com>
2021-06-01 18:55:15 -0400

    map function to render the pledges

--- src/components/Pledge.js
+++ src/components/Pledge.js
@@ -1,20 +1,30 @@
+import { useSelector } from 'react-redux';
+
 import PledgeContainer from '../styles/components/Pledge/PledgeContainer';
 
 const Pledge = () => {
+  const pledgeInfo = useSelector(state => state.data.info[0].modals.default.pledges);
+
   return (
-    <PledgeContainer htmlFor="">
-      <div className="pledge_header">
-        <input id="" type="radio" />
-        <h3 className="pledge_title"></h3>
-      </div>
-      <p className="pledge_description"></p>
-      {false &&
-        <div className="pledge_availability">
-          <span></span>
-          <p></p>
-        </div>
-      }
-    </PledgeContainer>
+    <>
+    {pledgeInfo.map(pledge => {
+      return (
+        <PledgeContainer htmlFor="">
+          <div className="pledge_header">
+            <input id="" type="radio" />
+            <h3 className="pledge_title"></h3>
+          </div>
+          <p className="pledge_description"></p>
+          {false && (
+            <div className="pledge_availability">
+              <span></span>
+              <p></p>
+            </div>
+          )}
+        </PledgeContainer>
+      );
+    })}
+    </>
   );
 }
 
