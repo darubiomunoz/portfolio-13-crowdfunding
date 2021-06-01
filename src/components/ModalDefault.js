0743a30c0514a63ec13961fffa03568c58fb61a2
Diego Rubio <darubiomunoz@gmail.com>
2021-06-01 17:21:02 -0400

    Jsx layout of the modal default

--- src/components/ModalDefault.js
+++ src/components/ModalDefault.js
@@ -1,15 +1,22 @@
+import { useSelector } from 'react-redux';
+
 import ModalDefaultContainer from '../styles/components/ModalDefault/ModalDefaultContainer';
 
+import closeIcon from '../assets/icons/icon-close-modal.svg';
+
 const ModalDefault = () => {
+  const defaultInfo = useSelector(state => state.data.info[0].modals.default);
+
   return (
     <ModalDefaultContainer>
       <main>
         <div>
-          <h1></h1>
+          <h1>{defaultInfo.title}</h1>
           <figure>
-            <img />
+            <img src={closeIcon} alt="Press enter to close the modal" />
           </figure>
         </div>
+        <p>{defaultInfo.subtitle}</p>
       </main>
     </ModalDefaultContainer>
   );
