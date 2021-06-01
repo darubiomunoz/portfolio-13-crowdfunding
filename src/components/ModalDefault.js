39adb05503458ad192fdbe53190812b338ddf94d
Diego Rubio <darubiomunoz@gmail.com>
2021-06-01 17:34:35 -0400

    Adding functionality to close modal in the defaul modal component

--- src/components/ModalDefault.js
+++ src/components/ModalDefault.js
@@ -1,11 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ModalDefaultContainer from '../styles/components/ModalDefault/ModalDefaultContainer';
 
 import closeIcon from '../assets/icons/icon-close-modal.svg';
 
+import { closeDefault } from '../features/modals/modalSlice';
+
 const ModalDefault = () => {
+  const dispatch = useDispatch();
   const defaultInfo = useSelector(state => state.data.info[0].modals.default);
+  const defaultStatus = useSelector(state => state.modals.default);
+
+  const handleClosingClick = () => {
+    if (defaultStatus === true) dispatch(closeDefault());
+  }
 
   return (
     <ModalDefaultContainer>
@@ -13,7 +21,7 @@ const ModalDefault = () => {
         <div>
           <h1 className="default_title">{defaultInfo.title}</h1>
           <figure>
-            <img src={closeIcon} alt="Press enter to close the modal" />
+            <img src={closeIcon} alt="Press enter to close the modal" onClick={handleClosingClick} />
           </figure>
         </div>
         <p className="default_subtitle">{defaultInfo.subtitle}</p>
