dea5413f7910a511741ef43771eea1f0127158a0
Diego Rubio <darubiomunoz@gmail.com>
2021-06-01 15:05:26 -0400

    Adding the modals reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import dataReducer from '../features/data/dataSlice';
+import modalsReducer from '../features/modals/modalSlice';
 
 export const store = configureStore({
   reducer: {
-    data: dataReducer
+    data: dataReducer,
+    modals: modalsReducer
   },
 });
