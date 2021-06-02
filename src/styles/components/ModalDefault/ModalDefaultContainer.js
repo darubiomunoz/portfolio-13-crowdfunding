a986dc6f8dad6a4c08eaa3a58165e57a661461da
Diego Rubio <darubiomunoz@gmail.com>
2021-06-01 22:45:33 -0400

    modal default style to scroll

--- src/styles/components/ModalDefault/ModalDefaultContainer.js
+++ src/styles/components/ModalDefault/ModalDefaultContainer.js
@@ -1,21 +1,24 @@
 import styled from 'styled-components';
 
-import { borderRadiusSmall, colorOption3, colorOption3_5, colorOption5, fontSizeRegular, fontSizeSmall, fontWeightBold, spacingSmall } from '../../variables';
+import { borderRadiusSmall, colorOption3, colorOption3_5, colorOption5, fontSizeRegular, fontSizeSmall, fontWeightBold, spacingSmall, zIndexModal } from '../../variables';
 
 const ModalDefaultContainer = styled.div`
   width: 100vw;
-  max-width: 100vw;
-  min-height: 100%;
+  height: fit-content;
+  max-height: 100vh;
+  padding: ${spacingSmall} 0;
   position: fixed;
   top: 0;
   left: 0;
   display: flex;
   justify-content: center;
-  align-items: center;
   background-color: ${colorOption3_5};
+  overflow-y: auto;
+  z-index: ${zIndexModal};
 
   main {
     width: 90%;
+    max-height: fit-content;
     padding: ${spacingSmall};
     display: flex;
     flex-direction: column;
