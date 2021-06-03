13942ef285190f88e5a4bd7300e49e1f752f63af
Diego Rubio <darubiomunoz@gmail.com>
2021-06-03 00:02:00 -0400

    pledge footer styles

--- src/components/Pledge.js
+++ src/components/Pledge.js
@@ -10,30 +10,77 @@ const Pledge = () => {
     {pledgeInfo.map(pledge => {
       return (
         <PledgeContainer htmlFor={pledge.title}>
-          <div className="pledge_header">
-            <input id={pledge.title} type="radio" name="pledge" disabled={pledge.daysLeft === "0" ? true : false}/>
-            <div className="header_titles">
-              <h3 className={pledge.daysLeft !== "0" ? "pledge_title" : "pledge_title-disabled"}>{pledge.title}</h3>
-              {pledge.subtitle && (
-                <h4 className={pledge.daysLeft !== "0" ?"pledge_subtitle" : "pledge_subtitle-disabled"}>{pledge.subtitle}</h4>
-              )}
-            </div>
-          </div>
-          <p className={pledge.daysLeft !== "0" ? "pledge_description" : "pledge_description-disabled"}>{pledge.description}</p>
-          {pledge.daysLeft && (
-            <div className="pledge_availability">
-              <span className={pledge.daysLeft !== "0" ? "pledge_daysleft" : "pledge_daysleft-disabled"}>{pledge.daysLeft}</span>
-              <p className={pledge.daysLeft !== "0" ? "availability_text" : "availability_text-disabled"}>left</p>
-            </div>
-          )}
-          {pledge.end.length > 0 && (
-            <div className="pledge_footer">
-              <h3 className="footer_title">{pledge.end[0]}</h3>
-              <div className="footer_fields">
-                <input type="number" placeholder={pledge.end[1]} />
-                <button type="button">{pledge.end[2]}</button>
+          <section className="pledge_body">
+            <div className="pledge_header">
+              <input
+                id={pledge.title}
+                type="radio"
+                name="pledge"
+                disabled={pledge.daysLeft === "0" ? true : false}
+              />
+              <div className="header_titles">
+                <h3
+                  className={
+                    pledge.daysLeft !== "0"
+                      ? "pledge_title"
+                      : "pledge_title-disabled"
+                  }
+                >
+                  {pledge.title}
+                </h3>
+                {pledge.subtitle && (
+                  <h4
+                    className={
+                      pledge.daysLeft !== "0"
+                        ? "pledge_subtitle"
+                        : "pledge_subtitle-disabled"
+                    }
+                  >
+                    {pledge.subtitle}
+                  </h4>
+                )}
               </div>
             </div>
+            <p
+              className={
+                pledge.daysLeft !== "0"
+                  ? "pledge_description"
+                  : "pledge_description-disabled"
+              }
+            >
+              {pledge.description}
+            </p>
+            {pledge.daysLeft && (
+              <div className="pledge_availability">
+                <span
+                  className={
+                    pledge.daysLeft !== "0"
+                      ? "pledge_daysleft"
+                      : "pledge_daysleft-disabled"
+                  }
+                >
+                  {pledge.daysLeft}
+                </span>
+                <p
+                  className={
+                    pledge.daysLeft !== "0"
+                      ? "availability_text"
+                      : "availability_text-disabled"
+                  }
+                >
+                  left
+                </p>
+              </div>
+            )}
+          </section>
+          {pledge.end.length > 0 && (
+            <section className="pledge_footer">
+              <h3 className="footer_title-pledge">{pledge.end[0]}</h3>
+              <div className="footer_fields">
+                <input className="fields_amount-pledge" type="number" placeholder={pledge.end[1]} />
+                <button className="fields_button-pledge" type="button">{pledge.end[2]}</button>
+              </div>
+            </section>
           )}
         </PledgeContainer>
       );
--- src/styles/components/Pledge/PledgeContainer.js
+++ src/styles/components/Pledge/PledgeContainer.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
-import { borderRadiusSmall, boxShadow, colorOption1, colorOption3, fontSizeRegular, fontSizeSmall, fontWeightBold, spacingSmall, colorOption1_5, colorOption4_5, colorOption3_5 } from '../../variables';
+import { borderRadiusSmall, boxShadow, colorOption1, colorOption3, fontSizeRegular, fontSizeSmall, fontWeightBold, spacingSmall, colorOption1_5, colorOption4_5, colorOption3_5, fontWeightNormal, colorOption5, borderRadiusRegular, spacingRegular } from '../../variables';
 
 const PledgeContainer = styled.label`
-  padding: ${spacingSmall};
   display: flex;
   flex-direction: column;
   row-gap: ${spacingSmall};
@@ -11,74 +10,120 @@ const PledgeContainer = styled.label`
   box-shadow: ${boxShadow};
 
   :focus-within {
-    border: .2rem solid ${colorOption1};
+    border: 0.2rem solid ${colorOption1};
   }
 
-  .pledge_header {
-    display: flex;
-    align-items: center;
+  .pledge_body {
+    padding: ${spacingSmall};
 
-    .header_titles {
-      margin-left: ${spacingSmall};
+    .pledge_header {
       display: flex;
-      flex-direction: column;
+      align-items: center;
 
-      .pledge_title {
-        font-size: ${fontSizeSmall};
-        font-weight: ${fontWeightBold};
-      }
+      .header_titles {
+        margin-left: ${spacingSmall};
+        display: flex;
+        flex-direction: column;
 
-      .pledge_title-disabled {
-        font-size: ${fontSizeSmall};
-        font-weight: ${fontWeightBold};
-        color: ${colorOption4_5};
-      }
+        .pledge_title {
+          font-size: ${fontSizeSmall};
+          font-weight: ${fontWeightBold};
+        }
 
-      .pledge_subtitle {
-        font-size: ${fontSizeSmall};
-        color: ${colorOption1};
-      }
+        .pledge_title-disabled {
+          font-size: ${fontSizeSmall};
+          font-weight: ${fontWeightBold};
+          color: ${colorOption4_5};
+        }
 
-      .pledge_subtitle-disabled {
-        font-size: ${fontSizeSmall};
-        color: ${colorOption1_5};
+        .pledge_subtitle {
+          font-size: ${fontSizeSmall};
+          color: ${colorOption1};
+        }
+
+        .pledge_subtitle-disabled {
+          font-size: ${fontSizeSmall};
+          color: ${colorOption1_5};
+        }
       }
     }
-  }
 
-  .pledge_description {
-    font-size: ${fontSizeSmall};
-    color: ${colorOption3};
-  }
-
-  .pledge_description-disabled {
-    font-size: ${fontSizeSmall};
-    color: ${colorOption3_5};
-  }
-
-  .pledge_availability {
-    display: flex;
-    column-gap: ${spacingSmall};
-
-    .pledge_daysleft {
-      font-size: ${fontSizeRegular};
-      font-weight: ${fontWeightBold};
-    }
-
-    .pledge_daysleft-disabled {
-      font-size: ${fontSizeRegular};
-      font-weight: ${fontWeightBold};
-      color: ${colorOption4_5};
-    }
-
-    .availability_text {
+    .pledge_description {
       font-size: ${fontSizeSmall};
       color: ${colorOption3};
     }
 
-    .availability_text-disabled {
+    .pledge_description-disabled {
       font-size: ${fontSizeSmall};
       color: ${colorOption3_5};
+    }
+
+    .pledge_availability {
+      display: flex;
+      column-gap: ${spacingSmall};
+
+      .pledge_daysleft {
+        font-size: ${fontSizeRegular};
+        font-weight: ${fontWeightBold};
+      }
+
+      .pledge_daysleft-disabled {
+        font-size: ${fontSizeRegular};
+        font-weight: ${fontWeightBold};
+        color: ${colorOption4_5};
+      }
+
+      .availability_text {
+        font-size: ${fontSizeSmall};
+        color: ${colorOption3};
+      }
+
+      .availability_text-disabled {
+        font-size: ${fontSizeSmall};
+        color: ${colorOption3_5};
+      }
+    }
+  }
+
+  .pledge_footer {
+    width: 100%;
+    padding: ${spacingSmall};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: ${spacingSmall};
+    border-top: 0.1rem solid ${colorOption3_5};
+
+    .footer_title-pledge {
+      font-size: ${fontSizeSmall};
+      font-weight: ${fontWeightNormal};
+      color: ${colorOption3};
+    }
+
+    .footer_fields {
+      width: 100%;
+      display: flex;
+      justify-content: space-evenly;
+
+      .fields_amount-pledge {
+        width: 40%;
+        padding: ${spacingSmall} ${spacingRegular};
+        border: .1rem solid ${colorOption3_5};
+        border-radius: ${borderRadiusRegular};
+        font-size: ${fontSizeSmall};
+        font-weight: ${fontWeightBold};
+        background-color: transparent;
+        outline: none;
+      }
+
+      .fields_button-pledge {
+        padding: ${spacingSmall} ${spacingRegular};
+        border: none;
+        border-radius: ${borderRadiusRegular};
+        font-size: ${fontSizeSmall};
+        background-color: ${colorOption1};
+        color: ${colorOption5};
+      }
     }
   }
 `;
