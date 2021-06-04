4c3f29fd7fe6e6986deafec453da31f8d4eada3c
Diego Rubio <darubiomunoz@gmail.com>
2021-06-04 16:12:30 -0400

    Styles of the modal success component

--- src/components/ModalSuccess.js
+++ src/components/ModalSuccess.js
@@ -1,15 +1,22 @@
+import { useSelector } from 'react-redux';
+
 import ModalSuccessContainer from '../styles/components/ModalSuccess/ModalSuccessContainer';
 
+import successLogo from '../assets/icons/icon-check.svg';
+
 const ModalSuccess = () => {
+  const modalSuccessInfo = useSelector(state => state.data.info[0].modals.successful);
+  console.log(modalSuccessInfo);
+
   return (
     <ModalSuccessContainer>
       <div className="modalsuccess_body">
         <figure className="modalsuccess_image-container">
-          <img className="modalsuccess_image" src="" alt="" />
+          <img className="modalsuccess_image" src={successLogo} alt="Checked Icon" />
         </figure>
-        <h3 className="modalsuccess_title"></h3>
-        <p className="modalsuccess_description"></p>
-        <button className="modalsuccess_button" type="button"></button>
+        <h3 className="modalsuccess_title">{modalSuccessInfo.title}</h3>
+        <p className="modalsuccess_description">{modalSuccessInfo.description}</p>
+        <button className="modalsuccess_button" type="button">{modalSuccessInfo.button}</button>
       </div>
     </ModalSuccessContainer>
   );
--- src/styles/components/ModalSuccess/ModalSuccessContainer.js
+++ src/styles/components/ModalSuccess/ModalSuccessContainer.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { spacingSmall, colorOption3_5, zIndexModal } from '../../variables';
+import { spacingSmall, colorOption3_5, zIndexModal, colorOption5, borderRadiusSmall, spacingRegular, fontSizeRegular, fontWeightBold, fontSizeSmall, colorOption3, borderRadiusRegular, colorOption1 } from '../../variables';
 
 const ModalSuccessContainer = styled.div`
   width: 100vw;
@@ -14,10 +14,50 @@ const ModalSuccessContainer = styled.div`
   justify-content: center;
   align-items: center;
   background-color: ${colorOption3_5};
-  overflow-y: auto;
+  overflow-y: hidden;
   z-index: ${zIndexModal};
 
-  
+  .modalsuccess_body {
+    width: 90%;
+    height: fit-content;
+    padding: ${spacingRegular};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: ${spacingSmall};
+    border-radius: ${borderRadiusSmall};
+    background-color: ${colorOption5};
+
+    .modalsuccess_image-container {
+      width: 3rem;
+      height: 3rem;
+    }
+
+    .modalsuccess_image {
+      width: 100%;
+      height: 100%;
+    }
+
+    .modalsuccess_title {
+      font-size: ${fontSizeRegular};
+      font-weight: ${fontWeightBold};
+    }
+
+    .modalsuccess_description {
+      text-align: center;
+      font-size: ${fontSizeSmall};
+      color: ${colorOption3};
+    }
+
+    .modalsuccess_button {
+      padding: ${spacingSmall} ${spacingRegular};
+      border: none;
+      border-radius: ${borderRadiusRegular};
+      font-size: ${fontSizeSmall};
+      background-color: ${colorOption1};
+      color: ${colorOption5};
+    }
+  }
 `;
 
 export default ModalSuccessContainer;
