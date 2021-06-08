d3bf6f88aff7449a06066482f02ccff7269550a9
Diego Rubio <darubiomunoz@gmail.com>
2021-06-08 17:26:15 -0400

    Correcting the styles of the plan component

--- src/styles/components/Plan/PlanContainer.js
+++ src/styles/components/Plan/PlanContainer.js
@@ -1,6 +1,23 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { borderRadiusExtraSmall, borderRadiusRegular, boxShadow, colorOption1, colorOption1_5, colorOption3, colorOption3_5, colorOption4_5, colorOption5, fontSizeMedium, fontSizeSmall, fontWeightBold, fontWeightNormal, spacingRegular, spacingSmall } from '../../variables';
+import {
+  borderRadiusExtraSmall,
+  borderRadiusRegular,
+  boxShadow,
+  colorOption1,
+  colorOption1_5,
+  colorOption3,
+  colorOption3_5,
+  colorOption4_5,
+  colorOption5,
+  fontSizeMedium,
+  fontSizeSmall,
+  fontWeightBold,
+  fontWeightNormal,
+  spacingRegular,
+  spacingSmall,
+  device
+} from "../../variables";
 
 const PlanContainer = styled.div`
   width: 100%;
@@ -11,42 +28,71 @@ const PlanContainer = styled.div`
   border-radius: ${borderRadiusExtraSmall};
   box-shadow: ${boxShadow};
 
-  .plan_title {
+  .plan_title,
+  .plan_title-disabled {
     font-size: ${fontSizeSmall};
     font-weight: ${fontWeightBold};
   }
 
-  .plan_subtitle {
+  .plan_title-disabled {
+    color: ${colorOption4_5};
+  }
+
+  .plan_subtitle,
+  .plan_subtitle-disabled {
     font-size: ${fontSizeSmall};
     font-weight: ${fontWeightNormal};
     color: ${colorOption1};
   }
 
-  .plan_paragraph {
+  .plan_subtitle-disabled {
+    color: ${colorOption1_5};
+  }
+
+  .plan_paragraph,
+  .plan_paragraph-disabled {
     margin-top: ${spacingRegular};
     font-size: ${fontSizeSmall};
     font-weight: ${fontWeightNormal};
     color: ${colorOption3};
   }
 
-  .plan_counter {
+  .plan_paragraph-disabled {
+    color: ${colorOption3_5};
+  }
+
+  .plan_counter,
+  .plan_counter-disabled {
     margin-top: ${spacingRegular};
     display: flex;
     align-items: center;
 
-    .counter_number {
+    .counter_number,
+    .counter_number-disabled {
       font-size: ${fontSizeMedium};
       font-weight: ${fontWeightBold};
     }
 
-    .counter_text {
+    .counter_text,
+    .counter_text-disabled {
       margin-left: ${spacingSmall};
       font-size: ${fontSizeSmall};
       color: ${colorOption3};
     }
   }
 
-  .plan_button {
+  .plan_counter-disabled {
+    .counter_number-disabled {
+      color: ${colorOption4_5};
+    }
+
+    .counter_text-disabled {
+      color: ${colorOption3_5};
+    }
+  }
+
+  .plan_button,
+  .plan_button-disabled {
     margin-top: ${spacingRegular};
     padding: ${spacingSmall} ${spacingRegular};
     font-size: ${fontSizeSmall};
@@ -56,51 +102,8 @@ const PlanContainer = styled.div`
     color: ${colorOption5};
   }
 
-  .plan_title-disabled {
-    font-size: ${fontSizeSmall};
-    font-weight: ${fontWeightBold};
-    color: ${colorOption4_5};
-  }
-
-  .plan_subtitle-disabled {
-    font-size: ${fontSizeSmall};
-    font-weight: ${fontWeightNormal};
-    color: ${colorOption1_5};
-  }
-
-  .plan_paragraph-disabled {
-    margin-top: ${spacingRegular};
-    font-size: ${fontSizeSmall};
-    font-weight: ${fontWeightNormal};
-    color: ${colorOption3_5};
-  }
-
-  .plan_counter-disabled {
-    margin-top: ${spacingRegular};
-    display: flex;
-    align-items: center;
-
-    .counter_number-disabled {
-      font-size: ${fontSizeMedium};
-      font-weight: ${fontWeightBold};
-      color: ${colorOption4_5};
-    }
-
-    .counter_text-disabled {
-      margin-left: ${spacingSmall};
-      font-size: ${fontSizeSmall};
-      color: ${colorOption3_5};
-    }
-  }
-
   .plan_button-disabled {
-    margin-top: ${spacingRegular};
-    padding: ${spacingSmall} ${spacingRegular};
-    font-size: ${fontSizeSmall};
-    border: none;
-    border-radius: ${borderRadiusRegular};
     background-color: ${colorOption3_5};
-    color: ${colorOption5};
   }
 
   .screen {
@@ -112,6 +115,10 @@ const PlanContainer = styled.div`
     bottom: 0;
     left: 0;
     background-color: transparent;
+  }
+
+  @media ${device.width480} {
+
   }
 `;
 
