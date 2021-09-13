20851643558243c75f7c1935d90dbd956441ddfe
Diego Rubio <darubiomunoz@gmail.com>
2021-09-13 12:14:32 -0300

    Tests are running

--- src/__test__/components/Navbar.test.js
+++ src/__test__/components/Navbar.test.js
@@ -3,10 +3,11 @@ import { render } from '@testing-library/react';
 import Navbar from '../../components/Navbar';
 
 describe('Navbar component tests', () => {
-  test('If it renders', () => {
+  it('If it renders', () => {
     const { getByTestId } = render(<Navbar />);
     const logo = getByTestId('logoTestId');
 
     expect(logo).toBeTruthy();
-  })
+  });
+  
 });
